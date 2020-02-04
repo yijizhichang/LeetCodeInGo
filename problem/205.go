d04fe4e5788120d123688ceab395634a9066a49c
gfy <guofeiyang@xiaoduotech.com>
2020-02-04 11:20:54 +0800

    add 69,70,79,190,205

---
+++ problem/205.go
@@ -0,0 +1,70 @@
+package problem
+
+/*
+给定两个字符串 s 和 t，判断它们是否是同构的。
+
+如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+
+所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+
+示例 1:
+
+输入: s = "egg", t = "add"
+输出: true
+示例 2:
+
+输入: s = "foo", t = "bar"
+输出: false
+示例 3:
+
+输入: s = "paper", t = "title"
+输出: true
+说明:
+你可以假设 s 和 t 具有相同的长度。
+ */
+
+func isIsomorphic(s string, t string) bool {
+	s2t := make(map[string]string)
+	for index, this := range s{
+		if value, ok := s2t[string(this)]; ok {
+			if value != string(t[index]) {
+				return false
+			}
+		} else {
+			s2t[string(this)] = string(t[index])
+		}
+	}
+	t2s := make(map[string]string)
+	for index, this := range t{
+		if value, ok := t2s[string(this)]; ok {
+			if value != string(s[index]) {
+				return false
+			}
+		} else {
+			t2s[string(this)] = string(s[index])
+		}
+	}
+	return true
+}
+
+/*
+记录个数
+func isIsomorphic(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	m1 := make([]int, 256)
+	m2 := make([]int, 256)
+
+	for i := 0; i < len(s); i++ {
+		if m1[int(s[i])] != m2[int(t[i])] {
+			return false
+		}
+
+		m1[int(s[i])] = i + 1
+		m2[int(t[i])] = i + 1
+	}
+	return true
+}
+ */
---
+++ problem/69.go
@@ -0,0 +1,41 @@
+package problem
+
+/*
+	实现 int sqrt(int x) 函数。
+
+	计算并返回 x 的平方根，其中 x 是非负整数。
+
+	由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+	示例 1:
+
+	输入: 4
+	输出: 2
+	示例 2:
+
+	输入: 8
+	输出: 2
+	说明: 8 的平方根是 2.82842...,
+	     由于返回类型是整数，小数部分将被舍去。
+ */
+
+// 二分
+func mySqrt(x int) int {
+	if x == 0 || x == 1{
+		return x
+	}
+	start := 1
+	end := x
+	for start<=end{
+		// 第一次肯定是靠左
+		mid := start+(end-start)/2 // 等同start-start/2 + end/2 -> (start+end)/2
+		if mid*mid == x{
+			return mid
+		}else if mid*mid <x{
+			start = mid+1
+		}else {
+			end = mid-1
+		}
+	}
+	return start-1
+}
