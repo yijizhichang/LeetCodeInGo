136d56424e5377d91d85b6efda98393d0807dfd9
gfy <502270682@qq.com>
2021-01-05 00:13:39 +0800

    b-tree 114 116 226

---
+++ problem/binary_tree/116.go
@@ -0,0 +1,49 @@
+package binary_tree
+
+/*
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+
+
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+*/
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	connectHelper(root.Left, root.Right)
+	return root
+}
+
+func connectHelper(node1, node2 *Node) {
+	if node1 == nil || node2 == nil {
+		return
+	}
+	node1.Next = node2
+	connectHelper(node1.Left, node1.Right)
+	connectHelper(node1.Right, node2.Left)
+	connectHelper(node2.Left, node2.Right)
+	return
+}
