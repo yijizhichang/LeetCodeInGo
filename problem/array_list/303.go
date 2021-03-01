3c3b4e0d0eca9c715a2265144902a956d4f6f2dd
gfy <502270682@qq.com>
2021-03-01 23:22:44 +0800

    add 303

---
+++ problem/array_list/303.go
@@ -0,0 +1,56 @@
+package array_list
+
+/*
+给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+
+
+示例：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+
+*/
+
+type NumArray struct {
+	sums []int
+}
+
+func Constructor(nums []int) NumArray {
+	sums := make([]int, len(nums))
+	for i, v := range nums {
+		if i == 0 {
+			sums[0] = v
+		} else {
+			sums[i] += sums[i-1] + v
+		}
+	}
+	return NumArray{sums: sums}
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	if i > 0 {
+		return this.sums[j] - this.sums[i-1]
+	} else {
+		return this.sums[j]
+	}
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
