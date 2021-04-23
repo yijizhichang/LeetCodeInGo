481860c548f3d9efced523702b6250700cf13929
gfy <502270682@qq.com>
2021-04-23 23:06:11 +0800

    add 456

---
+++ problem/stack/456.go
@@ -0,0 +1,55 @@
+package stack
+
+import "math"
+
+/*
+给你一个整数数组 nums ，数组中共有 n 个整数。132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+
+如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
+
+
+
+进阶：很容易想到时间复杂度为 O(n^2) 的解决方案，你可以设计一个时间复杂度为 O(n logn) 或 O(n) 的解决方案吗？
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4]
+输出：false
+解释：序列中不存在 132 模式的子序列。
+示例 2：
+
+输入：nums = [3,1,4,2]
+输出：true
+解释：序列中有 1 个 132 模式的子序列： [1, 4, 2] 。
+
+*/
+
+/*
+、
+*/
+
+// 单调栈
+func find132pattern(nums []int) bool {
+	n := len(nums)
+	candidateK := []int{nums[n-1]}
+	maxK := math.MinInt64
+
+	for i := n - 2; i >= 0; i-- {
+		if nums[i] < maxK {
+			return true
+		}
+		// 找到当前序列2中满足小于当前i的最大值
+		for len(candidateK) > 0 && nums[i] > candidateK[len(candidateK)-1] {
+			maxK = candidateK[len(candidateK)-1]
+			candidateK = candidateK[:len(candidateK)-1]
+		}
+		// 当前值大于栈顶值，也就是2的最大值，所以一定可以把他加入栈顶，作为下一个3
+		if nums[i] > maxK {
+			candidateK = append(candidateK, nums[i])
+		}
+	}
+
+	return false
+}
