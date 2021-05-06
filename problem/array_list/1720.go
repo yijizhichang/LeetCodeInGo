2986061244abf01845671ffd2ca7008df2703fe2
gfy <502270682@qq.com>
2021-05-06 22:26:13 +0800

    add 1720

---
+++ problem/array_list/1720.go
@@ -0,0 +1,29 @@
+package array_list
+
+/*
+未知 整数数组 arr 由 n 个非负整数组成。
+
+经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
+
+给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
+
+请解码返回原数组 arr 。可以证明答案存在并且是唯一的。
+
+
+
+示例 1：
+
+输入：encoded = [1,2,3], first = 1
+输出：[1,0,2,1]
+解释：若 arr = [1,0,2,1] ，那么 first = 1 且 encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+
+*/
+
+func decode(encoded []int, first int) []int {
+	ret := make([]int, len(encoded)+1)
+	ret[0] = first
+	for i, num := range encoded {
+		ret[i+1] = num ^ ret[i]
+	}
+	return ret
+}
