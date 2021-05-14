58e0afe0154a52184c6bcc77c0fe41e2fa339fed
feiyangguo <feiyang.guo@iglooinsure.com>
2021-05-14 18:37:20 +0800

    add 12

---
+++ problem/int/12.go
@@ -0,0 +1,56 @@
+package int
+
+/*
+罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给你一个整数，将其转为罗马数字。
+
+ */
+
+var valueSymbols = []struct {
+	value  int
+	symbol string
+}{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func intToRoman(num int) string {
+	roman := []byte{}
+	for _, vs := range valueSymbols {
+		for num >= vs.value {
+			num -= vs.value
+			roman = append(roman, vs.symbol...)
+		}
+		if num == 0 {
+			break
+		}
+	}
+	return string(roman)
+}
