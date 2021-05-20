b752082d22be39e3933607ea27e3ff811c945eda
feiyangguo <feiyang.guo@iglooinsure.com>
2021-05-20 13:59:37 +0800

    add 692

---
+++ problem/sort/692.go
@@ -0,0 +1,76 @@
+package sort
+
+import (
+	"container/heap"
+	"sort"
+)
+
+/*
+给一非空的单词列表，返回前 k 个出现次数最多的单词。
+
+返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+
+示例 1：
+
+输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+输出: ["i", "love"]
+解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+    注意，按字母顺序 "i" 在 "love" 之前。
+ 
+
+示例 2：
+
+输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+输出: ["the", "is", "sunny", "day"]
+解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+    出现次数依次为 4, 3, 2 和 1 次。
+
+ */
+
+func topKFrequent(words []string, k int) []string {
+	cnt := map[string]int{}
+	for _, w := range words {
+		cnt[w]++
+	}
+	uniqueWords := make([]string, 0, len(cnt))
+	for w := range cnt {
+		uniqueWords = append(uniqueWords, w)
+	}
+	sort.Slice(uniqueWords, func(i, j int) bool {
+		s, t := uniqueWords[i], uniqueWords[j]
+		return cnt[s] > cnt[t] || cnt[s] == cnt[t] && s < t
+	})
+	return uniqueWords[:k]
+}
+
+// 优先队列
+type pair struct {
+	w string
+	c int
+}
+type hp []pair
+func (h hp) Len() int            { return len(h) }
+func (h hp) Less(i, j int) bool  { a, b := h[i], h[j]; return a.c < b.c || a.c == b.c && a.w > b.w }
+func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
+func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+
+func topKFrequent(words []string, k int) []string {
+	cnt := map[string]int{}
+	for _, w := range words {
+		cnt[w]++
+	}
+	h := &hp{}
+	for w, c := range cnt {
+		heap.Push(h, pair{w, c})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	ans := make([]string, k)
+	for i := k - 1; i >= 0; i-- {
+		ans[i] = heap.Pop(h).(pair).w
+	}
+	return ans
+}
+
