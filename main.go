eeaf2ea4c18b3f3846874fc8f5907f8917d35ff7
gfy <502270682@qq.com>
2021-05-31 23:07:17 +0800

    add 342

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	fmt.Println("test here!")
+	fmt.Println(fmt.Sprintf("%f", math.Sqrt(float64(16))))
+
+	fmt.Println(math.Sqrt(float64(16)))
 }
