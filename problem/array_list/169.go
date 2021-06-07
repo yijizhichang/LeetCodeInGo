b2e1fe418bfb1dc2c8e8335bd2de8abfe3188507
gfy <502270682@qq.com>
2021-06-07 22:31:18 +0800

    add 169

--- problem/array_list/169.go
+++ problem/array_list/169.go
@@ -1,4 +1,4 @@
-package problem
+package array_list
 
 /*
 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
@@ -30,4 +30,21 @@ func majorityElement(nums []int) int {
 		}
 	}
 	return 0
+}
+
+// 摩尔投票算法
+func majorityElement(nums []int) int {
+	target := 0
+	count := 0
+	for _, num := range nums {
+		if count == 0 {
+			target = num
+		}
+		if num == target {
+			count++
+		} else {
+			count--
+		}
+	}
+	return target
 }
