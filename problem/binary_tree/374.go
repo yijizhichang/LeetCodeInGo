0420bd811169f516a932a2b8c86128b0572a5289
gfy <502270682@qq.com>
2021-06-14 10:12:07 +0800

    binary search: 374 猜数字

---
+++ problem/binary_tree/374.go
@@ -0,0 +1,41 @@
+package binary_tree
+
+import "sort"
+
+/*
+猜数字游戏的规则如下：
+
+每轮游戏，我都会从 1 到 n 随机选择一个数字。 请你猜选出的是哪个数字。
+如果你猜错了，我会告诉你，你猜测的数字比我选出的数字是大了还是小了。
+你可以通过调用一个预先定义好的接口 int guess(int num) 来获取猜测结果，返回值一共有 3 种可能的情况（-1，1 或 0）：
+
+-1：我选出的数字比你猜的数字小 pick < num
+1：我选出的数字比你猜的数字大 pick > num
+0：我选出的数字和你猜的数字一样。恭喜！你猜对了！pick == num
+返回我选出的数字。
+
+*/
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ */
+
+func guess(num int) int
+
+func guessNumber(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := (right-left)/2 + left
+		if ret := guess(mid); ret == 0 {
+			return mid
+		} else if ret == -1 {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
