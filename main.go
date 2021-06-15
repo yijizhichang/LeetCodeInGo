8dc4bf0c16fddcf37d18dec6812507747c752f6a
gfy <502270682@qq.com>
2021-06-15 22:00:14 +0800

    binary_search: 852

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	fmt.Println("test here!")
-	fmt.Println(fmt.Sprintf("%f", math.Sqrt(float64(16))))
-
-	fmt.Println(math.Sqrt(float64(16)))
 }
