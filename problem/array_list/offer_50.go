8c08699f00b3b2a6ce3c02173a53294a9a1e8010
gfy <502270682@qq.com>
2021-06-18 23:17:54 +0800

    hash: 找到首个只出现了一次的字符

---
+++ problem/array_list/offer_50.go
@@ -0,0 +1,27 @@
+package problem
+
+/*
+在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+
+示例:
+
+s = "abaccdeff"
+返回 "b"
+
+s = ""
+返回 " "
+
+*/
+
+func firstUniqChar(s string) byte {
+	cnt := [26]int{}
+	for _, ch := range s {
+		cnt[ch-'a']++
+	}
+	for i, ch := range s {
+		if cnt[ch-'a'] == 1 {
+			return s[i]
+		}
+	}
+	return ' '
+}
