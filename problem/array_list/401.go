6d93b25fbb12d10746557bf8409e4f33972d3455
gfy <502270682@qq.com>
2021-06-21 23:00:52 +0800

    bit watch

---
+++ problem/array_list/401.go
@@ -0,0 +1,34 @@
+package array_list
+
+import (
+	"fmt"
+	"math/bits"
+)
+
+/*
+小时不会以零开头：
+
+例如，"01:00" 是无效的时间，正确的写法应该是 "1:00" 。
+分钟必须由两位数组成，可能会以零开头：
+
+例如，"10:2" 是无效的时间，正确的写法应该是 "10:02" 。
+
+
+示例 1：
+
+输入：turnedOn = 1
+输出：["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+
+*/
+
+// 枚举
+func readBinaryWatch(turnedOn int) (ans []string) {
+	for h := uint8(0); h < 12; h++ {
+		for m := uint8(0); m < 60; m++ {
+			if bits.OnesCount8(h)+bits.OnesCount8(m) == turnedOn {
+				ans = append(ans, fmt.Sprintf("%d:%02d", h, m))
+			}
+		}
+	}
+	return
+}
