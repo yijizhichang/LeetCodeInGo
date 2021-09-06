20a28f766348f950b17c4ef5cde0c21e0575b838
gfy <502270682@qq.com>
2021-09-06 21:17:20 +0800

    binary_search: add 704

---
+++ problem/binary_tree/704.go
@@ -0,0 +1,33 @@
+package binary_tree
+
+/*
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+
+*/
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		if nums[mid] < target {
+			left = mid
+		} else if nums[mid] == target {
+			return mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
